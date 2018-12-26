a740286eeb72643c93d3dee4e97cbd49b67f4c43
Lukas <z.lukas.1996@web.de>
2018-12-26 23:54:34 +0100

    starting the Tutorial

--- app/src/main/java/com/lukascomp/smashboard/MainActivity.java
+++ app/src/main/java/com/lukascomp/smashboard/MainActivity.java
@@ -2,12 +2,24 @@ package com.lukascomp.smashboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.smashboard.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goTOPlayerList(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        String message = "hello world";
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
